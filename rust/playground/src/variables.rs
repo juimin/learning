0f686c67114f9ec4f785d57898e77e91037ef381
JuiMin <d95wang@gmail.com>
2020-11-22 23:30:44 -0800

    add example about constants

--- rust/playground/src/variables.rs
+++ rust/playground/src/variables.rs
@@ -3,9 +3,20 @@ Variables and Mutability
 
 Variables
     - Immutable By Default
+    - The value that the variable points to cannot change
 
-
+Constants
+    - By definition immutable
+    - Type annotation required
+    - Can be declared in any scope
+    - Can only be set to a constant expression
+        - This is different from python where it can be set at run time
 */
+
+// Constants are all caps (standard)
+// underscores can be used in numeric literals for readability (this is still 100k)
+const BASIC_GLOBAL: u32 = 100_000;
+
 
 fn immutable_example() {
     println!("immutability example:");
@@ -24,7 +35,16 @@ fn immutable_example() {
     println!();
 }
 
+fn constants_example() {
+    const LOCAL_CONST: u8 = 12;
+    println!("constants example:");
+    println!("basic global example: {}", BASIC_GLOBAL);
+    println!("basic local example: {}", LOCAL_CONST);
+}
+
+
 pub fn main() {
     println!("==Variables Module==\n");
     immutable_example();
+    constants_example();
 }
