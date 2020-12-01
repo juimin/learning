598d37bd196f102c7173c5d0aaaf5c7f46df7c21
JuiMin <d95wang@gmail.com>
2020-11-30 21:01:20 -0800

    add enum definitions with types and mixed types

--- rust/playground/src/enums.rs
+++ rust/playground/src/enums.rs
@@ -19,6 +19,8 @@ if let
 pub fn main() {
     println!("\nEnumerations and all that Jazz\n");
     instantiate_example();
+    better_ipaddr_enum();
+    mixed_enum();
 }
 
 // Sample enum
@@ -43,3 +45,45 @@ fn instantiate_example() {
     println!("Six: {} - Four: {}", printip(six), printip(four));
 }
 
+// Associate each of the enum kinds with a value type
+// This eliminates the need for extra structs to specify an ip address
+
+// Otherwise it would be something like
+/*
+struct Ipaddr {
+    IpaddrKind: IpAddrKind::V4,
+    Value: String,
+}
+
+*/
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+fn better_ipaddr_enum() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    // Can we print these?
+    println!("These are the better ipaddr enums: {:?} {:?}", home, loopback);
+}
+
+#[derive(Debug)]
+enum MixedIpAddr {
+    // See how we can make the type a tuple
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+
+fn mixed_enum() {
+    let home = MixedIpAddr::V4(127, 0, 0, 1);
+
+    let loopback = MixedIpAddr::V6(String::from("::1"));
+
+    // Can we print these?
+    println!("These are the better mixed ipaddr enums: {:?} {:?}", home, loopback);
+}
