97ae485d18e574b5cec18d3296db5282394fa813
JuiMin <d95wang@gmail.com>
2020-12-02 23:41:44 -0800

    finish day 2 part 2

--- rust/advent/src/day2.rs
+++ rust/advent/src/day2.rs
@@ -23,6 +23,7 @@ fn file_contents_as_string(filename: &str) -> String {
 fn parse_contents(file_contents: &str) {
     // Get the valid password count
     let mut valid_total = 0;
+    let mut valid_total_part_2 = 0;
     for line in file_contents.lines() {
         let mut splits = line.split_whitespace();
         // Extract the segments from the line
@@ -38,17 +39,28 @@ fn parse_contents(file_contents: &str) {
         let max: i32 = range_split.next().unwrap().parse().expect("sucks to suck");
 
         let mut count = 0;
-        for letter in password.chars() {
+        let mut matches = 0;
+        for (index, letter) in password.chars().enumerate() {
             if letter == target {
                 count += 1;
+                if ((index as i32) + 1) == min {
+                    matches += 1;
+                }
+                if ((index as i32) + 1) == max {
+                    matches += 1;
+                }
             }
         }
 
         if count >= min && count <= max {
             valid_total += 1;
         }
+        if matches == 1 {
+            valid_total_part_2 += 1;
+        }
     }
     println!("Day 2 Part 1: Number of valid passwords {}", valid_total);
+    println!("Day 2 Part 2: Number of valid passwords {}", valid_total_part_2);
 }
 
 pub fn main() {
