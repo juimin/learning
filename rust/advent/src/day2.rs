ab20d2a939c5d6d1241577906ed95d7b819bce9f
JuiMin <d95wang@gmail.com>
2020-12-02 23:37:45 -0800

    finish day 2 part 1

--- rust/advent/src/day2.rs
+++ rust/advent/src/day2.rs
@@ -1,3 +1,57 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn file_contents_as_string(filename: &str) -> String {
+    let path = Path::new(filename);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut file_contents = String::new();
+    match file.read_to_string(&mut file_contents) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => return file_contents
+    }
+}
+
+fn parse_contents(file_contents: &str) {
+    // Get the valid password count
+    let mut valid_total = 0;
+    for line in file_contents.lines() {
+        let mut splits = line.split_whitespace();
+        // Extract the segments from the line
+        let p1 = splits.next();
+        let mut range_split = p1.unwrap().split("-");
+        let p2 = splits.next();
+        let target = p2.unwrap().chars().next().unwrap();
+        let p3 = splits.next();
+        let password = p3.unwrap();
+
+        // Get the range
+        let min: i32 = range_split.next().unwrap().parse().expect("sucks to suck");
+        let max: i32 = range_split.next().unwrap().parse().expect("sucks to suck");
+
+        let mut count = 0;
+        for letter in password.chars() {
+            if letter == target {
+                count += 1;
+            }
+        }
+
+        if count >= min && count <= max {
+            valid_total += 1;
+        }
+    }
+    println!("Day 2 Part 1: Number of valid passwords {}", valid_total);
+}
+
 pub fn main() {
-    
+    let file_contents = file_contents_as_string("./data/day2.txt");
+    parse_contents(&file_contents);
 }
