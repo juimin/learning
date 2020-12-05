a1b82e07a761cfff22276e446ab6b725020fe2a6
JuiMin <d95wang@gmail.com>
2020-12-04 22:58:55 -0800

    finish day 5 part 2

--- rust/advent/src/day5.rs
+++ rust/advent/src/day5.rs
@@ -5,9 +5,11 @@ fn get_row_number(input: &str) -> i32 {
     binary_search(128, 'B', input)
 }
 
+
 fn get_col_number(input: &str) -> i32 {
     binary_search(8, 'R', input)
 }
+
 
 fn binary_search(size: i32, high: char, input: &str) -> i32 {
     let mut min = 0;
@@ -24,11 +26,19 @@ fn binary_search(size: i32, high: char, input: &str) -> i32 {
         }
     }
     return mid
-} 
+}
+
 
 pub fn main() {
     let fc = util::file_contents_as_string("./data/day5.txt");
     let mut max_seat_id = 0;
+    // Compute colum xor sum
+    let mut xor_sum = 0;
+    for n in 1..9 {
+        xor_sum ^= n;
+    }
+
+    let mut counter: [i32; 128] = [xor_sum; 128];
 
     for line in fc.lines() {
         let len = line.len();
@@ -38,6 +48,22 @@ pub fn main() {
         if seat_id > max_seat_id {
             max_seat_id = seat_id;
         }
+        // Add to the counter
+        counter[(row as usize)] ^= col + 1;
     }
     println!("Day 5 Part 1: {}", max_seat_id);
+
+    println!("End state: {:?}", counter);
+    let mut in_middle = false;
+    for (row, value) in counter.iter().enumerate() {
+        if !in_middle && *value == 0 {
+            in_middle = true;
+        }
+        // The first one we find should be the correct one
+        if in_middle && *value != 0 {
+            let my_seat_id = (8 * (row as i32)) + value - 1;
+            println!("Day 5 Part 2: {}", my_seat_id);
+            return
+        }
+    }
 }
