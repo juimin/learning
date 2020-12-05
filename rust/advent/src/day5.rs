b63f53b0683d657095c477ca9cf594720de57338
JuiMin <d95wang@gmail.com>
2020-12-04 21:45:47 -0800

    start day 5 work

---
+++ rust/advent/src/day5.rs
@@ -0,0 +1,5 @@
+use crate::util;
+
+pub fn main() {
+    let fc = util::file_contents_as_string("./data/day5.txt");
+}
--- rust/advent/src/main.rs
+++ rust/advent/src/main.rs
@@ -6,6 +6,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     println!("Advent of Code!");
@@ -13,4 +14,5 @@ fn main() {
     day2::main();
     day3::main();
     day4::main();
+    day5::main();
 }
