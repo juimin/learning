4471b976b1148e305cb344e83fae9dbcd3e2f9be
JuiMin <d95wang@gmail.com>
2020-12-06 22:29:36 -0800

    formatting

--- rust/advent2020/src/challenges.rs
+++ rust/advent2020/src/challenges.rs
@@ -8,22 +8,19 @@ mod day5;
 mod day6;
 mod day7;
 
-pub fn run(day:u8, file: &str) {
+pub fn run(day: u8, file: &str) {
     if let Ok(lines) = adventlib::read_lines(file) {
         let results = match day {
             1 => day1::run(lines),
             2 => day2::run(lines),
-            3 => {
-                day3::run(lines)
-            },
+            3 => day3::run(lines),
             4 => day4::run(lines),
             5 => day5::run(lines),
             6 => day6::run(lines),
             7 => day7::run(lines),
-            _ => (-1, -1)
+            _ => (-1, -1),
         };
         println!("Day {} Part 1: {}", day, results.0);
         println!("Day {} Part 2: {}", day, results.1);
     }
-
 }
--- rust/advent2020/src/main.rs
+++ rust/advent2020/src/main.rs
@@ -9,24 +9,32 @@ fn main() {
         .version("0.1")
         .author("Derek W.")
         .about("Command Line program for 2020's advent of code")
-        .arg(Arg::with_name("day")
-            .help("Day with which to run")
-            .short("d")
-            .long("day")
-            .required(true)
-            .value_name("DAY")
-            .takes_value(true))
-        .arg(Arg::with_name("file")
-            .help("File path used for input")
-            .short("f")
-            .long("file")
-            .required(true)
-            .value_name("FILE")
-            .takes_value(true)
-    )
-    .get_matches();
+        .arg(
+            Arg::with_name("day")
+                .help("Day with which to run")
+                .short("d")
+                .long("day")
+                .required(true)
+                .value_name("DAY")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("file")
+                .help("File path used for input")
+                .short("f")
+                .long("file")
+                .required(true)
+                .value_name("FILE")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let day: u8 = args.value_of("day").unwrap_or("0").trim().parse().expect("Day needs to be a number");
+    let day: u8 = args
+        .value_of("day")
+        .unwrap_or("0")
+        .trim()
+        .parse()
+        .expect("Day needs to be a number");
     let file = args.value_of("file").unwrap_or("");
 
     challenges::run(day, file);
