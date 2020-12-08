6cfe98d5ee96b3043f2a008f630f11cf61bf5794
JuiMin <d95wang@gmail.com>
2020-12-07 21:54:42 -0800

    consistent day 7 part 1

--- rust/advent2020/src/challenges/day7.rs
+++ rust/advent2020/src/challenges/day7.rs
@@ -1,5 +1,9 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufReader, Lines};
+use std::vec::Vec;
+
+const TARGET_BAG: &str = "shiny gold";
 
 fn split_root_and_contents(line: &str) -> (&str, &str) {
     let mut root_and_contents = line.split("contain");
@@ -22,7 +26,10 @@ fn get_count_and_clean(bag_str: &str) -> (i32, &str) {
 
 // Assumes valid input
 pub fn run(lines: Lines<BufReader<File>>) -> (i64, i64) {
-    let result = (0, 0);
+    let mut result = (0, 0);
+
+    let mut part1_map: HashMap<String, HashSet<String>> = HashMap::new();
+
     for line in lines {
         if let Ok(line) = line {
             let (mut this_bag, contents) = split_root_and_contents(&line);
@@ -30,10 +37,32 @@ pub fn run(lines: Lines<BufReader<File>>) -> (i64, i64) {
             for c in contents.split(",") {
                 if !c.contains("no other bags") {
                     let (_, child_bag) = get_count_and_clean(c);
+                    // Tackle part 1
+                    // Here we map children to their parents
+                    let parents = part1_map.entry(String::from(child_bag)).or_insert(HashSet::new());
+                    parents.insert(String::from(this_bag));
                 }
             }
         }
     }
 
+    // Part 1 Processing
+    let mut seen: HashSet<String> = HashSet::new();
+    let mut todo: Vec<String> = Vec::new();
+    todo.push(String::from(TARGET_BAG));
+    while let Some(top) = todo.pop() {
+        match part1_map.get(&top) {
+            Some(parents) => {
+                for parent in parents {
+                    todo.push(String::from(parent));
+                }
+            }
+            None => ()
+        }
+        seen.insert(String::from(&top));
+    }
+
+    // Subtract one because we don't count the original bag
+    result.0 = seen.len() as i64 - 1;
     result
 }
