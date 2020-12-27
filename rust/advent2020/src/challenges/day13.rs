6826dbd9ade1bf6112b26a06b8be1dc8100e401d
JuiMin <d95wang@gmail.com>
2020-12-27 11:13:02 -0800

    finish day 13 part 1

--- rust/advent2020/src/challenges/day13.rs
+++ rust/advent2020/src/challenges/day13.rs
@@ -2,11 +2,38 @@ use std::fs::File;
 use std::io::{BufReader, Lines};
 
 pub fn run(lines: Lines<BufReader<File>>) -> (i64, i64) {
-    let results = (0,0);
-    for line in lines {
-        if let Ok(l) = line {
-            println!("DO STUFF");
+    let mut results = (i64::max_value(),0);
+
+    let mut part1_bus_id = 0;
+    let mut target = -1;
+    // Read inputs
+    for (idx, l) in lines.enumerate() {
+        if let Ok(line) = l {
+            if idx == 0 {
+                target = line.parse().unwrap();
+            }
+            if idx == 1 {
+                for t in line.split(",") {
+                    if t != "x" {
+                        let v: i64 = t.parse().unwrap();
+                        let lower_limit = v * (target / v);
+                        let upper_limit = v * ((target / v) + 1);
+                        if lower_limit == target {
+                            results.0 = 0;
+                            break
+                        }
+                        if (upper_limit - target) < results.0 {
+                            results.0 = upper_limit - target;
+                            part1_bus_id = v;
+                        }
+                    }
+                }
+            }
         }
     }
+
+    results.0 *= part1_bus_id;
+    println!("{}", target);
+
     results
 }
