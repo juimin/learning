46dc65f697ac5976c2999e9d4bfa2dd45e42ff62
JuiMin <d95wang@gmail.com>
2020-12-26 23:33:20 -0800

    fix

--- rust/advent2020/src/challenges/day16.rs
+++ rust/advent2020/src/challenges/day16.rs
@@ -4,7 +4,7 @@ use std::io::{BufReader, Lines};
 pub fn run(lines: Lines<BufReader<File>>) -> (i64, i64) {
     let results = (0,0);
     for line in lines {
-        let Ok(l) = line {
+        if let Ok(l) = line {
             println!("DO STUFF");
         }
     }
