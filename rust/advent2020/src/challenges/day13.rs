4989d02d38b1be9fba33bd414780a12be80463ae
JuiMin <d95wang@gmail.com>
2020-12-28 22:04:36 -0800

    brute force 13
    this is too slow but hey it works for now

--- rust/advent2020/src/challenges/day13.rs
+++ rust/advent2020/src/challenges/day13.rs
@@ -1,11 +1,17 @@
 use std::fs::File;
 use std::io::{BufReader, Lines};
+use std::collections::HashSet;
+use std::vec::Vec;
+use std::iter::FromIterator;
 
 pub fn run(lines: Lines<BufReader<File>>) -> (i64, i64) {
     let mut results = (i64::max_value(),0);
 
     let mut part1_bus_id = 0;
     let mut target = -1;
+
+    let mut ride_ids: Vec<i64> = Vec::new();
+
     // Read inputs
     for (idx, l) in lines.enumerate() {
         if let Ok(line) = l {
@@ -26,14 +32,38 @@ pub fn run(lines: Lines<BufReader<File>>) -> (i64, i64) {
                             results.0 = upper_limit - target;
                             part1_bus_id = v;
                         }
+                        ride_ids.push(v);
+                    } else {
+                        ride_ids.push(-1);
                     }
                 }
             }
         }
     }
 
+    let sorted_ids = Vec::from_iter(ride_ids.iter());
+
+    let mut multiplier = 1;
+    while results.1 == 0 {
+        let mut failed = false;
+        let start = sorted_ids[0] * multiplier;
+        for idx in 1..sorted_ids.len() {
+            if *sorted_ids[idx] != -1 {
+                let v = start + idx as i64;
+                if v % sorted_ids[idx] != 0 {
+                    failed = true;
+                    break;
+                }
+            }
+        }
+        if !failed {
+            results.1 = start;
+        }
+        multiplier += 1;
+    }
+
+
     results.0 *= part1_bus_id;
-    println!("{}", target);
 
     results
 }
