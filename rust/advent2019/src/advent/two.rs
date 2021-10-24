ff3239498cfe64237d54a47dc156fa287c7f7755
JuiMin <d95wang@gmail.com>
2021-10-24 01:34:45 -0700

    add more shit

--- rust/advent2019/src/advent/two.rs
+++ rust/advent2019/src/advent/two.rs
@@ -1,10 +1,36 @@
 use adventlib;
 
+fn run_intcode(intcode: &mut Vec<i64>) {
+    let mut op_code_index = 0;
+    while op_code_index < intcode.len() {
+        let command = intcode[op_code_index];
+        let a = intcode[op_code_index + 1];
+        let b = intcode[op_code_index + 2];
+        let location = intcode[op_code_index + 3];
+        match command {
+            1 => {
+                intcode[location as usize] = a + b;
+            },
+            2 => {
+                intcode[location as usize] = a * b;
+            },
+            _ => return
+        }
+
+        // step to the next opcode
+        op_code_index += 4
+    }
+}
+
+
 pub fn program_alarm(file: &String) {
     println!("1202 Program Alarm\n");
 
-    let values = adventlib::read_file_as_list_of_i64(file);
+    let mut intcode = adventlib::read_file_as_list_of_i64(file);
 
-    println!("{:?}", values);
+    run_intcode(&mut intcode);
+
+
+    println!("Position 0 value at termination: {}", intcode[0]);
 
 }
--- rust/advent2019/src/main.rs
+++ rust/advent2019/src/main.rs
@@ -22,19 +22,34 @@ fn main() {
             .value_name("day")
             .takes_value(true),
         )
+        .arg(
+            Arg::with_name("test")
+            .help("Whether or not to use test")
+            .short("t")
+            .required(false)
+            .value_name("test")
+            .takes_value(false)
+        )
         .get_matches();
     
     // Parse the args to get the data we need
     let day: u8 = args.value_of("day").unwrap_or("0")
         .trim().parse().expect("Day needs to be a number");
     let mut file = args.value_of("file").unwrap_or("").to_string();
+    let testing = args.is_present("test");
+
 
     // Log the desire of the user
     println!("Advent of Code 2019 Day {}:", day);
 
     if file.len() == 0 {
-        file = format!("./input_data/day{}.txt", day);
+        let mut dir = "./input_data/";
+        if testing {
+            dir = "./input_data/"
+        }
+        file = format!("{}day{}.txt", dir, day);
     }
+
     
     println!("Processing: {}", &file);
     advent::run(day, &file)
