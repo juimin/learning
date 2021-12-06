42f6032a0bff54129331665659df0e863668f813
juimin <d95wang@gmail.com>
2021-12-05 20:00:00 -0800

    2021 advent of code

---
+++ rust/advent/src/advent2021/mod.rs
@@ -0,0 +1,32 @@
+pub mod one;
+
+pub fn run(day: u8, file: &String) {
+    match day {
+        1 => one::rocket_equation(file),
+        2 => println!("Not implemented\n"),
+        3 => println!("Not implemented\n"),
+        4 => println!("Not implemented\n"),
+        5 => println!("Not implemented\n"),
+        6 => println!("Not implemented\n"),
+        7 => println!("Not implemented\n"),
+        8 => println!("Not implemented\n"),
+        9 => println!("Not implemented\n"),
+        10 => println!("Not implemented\n"),
+        11 => println!("Not implemented\n"),
+        12 => println!("Not implemented\n"),
+        13 => println!("Not implemented\n"),
+        14 => println!("Not implemented\n"),
+        15 => println!("Not implemented\n"),
+        16 => println!("Not implemented\n"),
+        17 => println!("Not implemented\n"),
+        18 => println!("Not implemented\n"),
+        19 => println!("Not implemented\n"),
+        20 => println!("Not implemented\n"),
+        21 => println!("Not implemented\n"),
+        22 => println!("Not implemented\n"),
+        23 => println!("Not implemented\n"),
+        24 => println!("Not implemented\n"),
+        25 => println!("Not implemented\n"),
+        _ => println!("Not implemented\n"),
+    }
+}
--- rust/advent/src/main.rs
+++ rust/advent/src/main.rs
@@ -1,12 +1,15 @@
 mod advent2019;
+mod advent2021;
 use clap::{App, Arg};
-
 
 fn run_for_year(year: i64, day: u8, file: &String) {
     match year {
         2019 => {
             advent2019::run(day, file);
-        },
+        }
+        2021 => {
+            advent2021::run(day, file);
+        }
         _ => {
             println!("No implementation for year {}", year);
         }
@@ -17,49 +20,55 @@ fn main() {
     let args = App::new("Advent of Code 2019")
         .version("0,1")
         .author("Derek W.")
-        .about("CLI for 2019 Advent of Code")
+        .about("CLI for Advent of Code")
         .arg(
             Arg::with_name("file")
                 .help("Path to inputs for this challenge")
                 .short("f")
                 .required(false)
                 .value_name("file")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("day")
-            .help("Day with which to run")
-            .short("d")
-            .required(true)
-            .value_name("day")
-            .takes_value(true),
+                .help("Day with which to run")
+                .short("d")
+                .required(true)
+                .value_name("day")
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("year")
-            .help("Advent of Code year")
-            .short("y")
-            .required(true)
-            .value_name("year")
-            .takes_value(true),
+                .help("Advent of Code year")
+                .short("y")
+                .required(true)
+                .value_name("year")
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("test")
-            .help("Whether or not to use test")
-            .short("t")
-            .required(false)
-            .value_name("test")
-            .takes_value(false)
+                .help("Whether or not to use test")
+                .short("t")
+                .required(false)
+                .value_name("test")
+                .takes_value(false),
         )
         .get_matches();
-    
     // Parse the args to get the data we need
-    let day: u8 = args.value_of("day").unwrap_or("0")
-        .trim().parse().expect("Day needs to be a number");
-    let year: i64 = args.value_of("year").unwrap_or("0")
-        .trim().parse().expect("Day needs to be a number");
+    let day: u8 = args
+        .value_of("day")
+        .unwrap_or("0")
+        .trim()
+        .parse()
+        .expect("Day needs to be a number");
+    let year: i64 = args
+        .value_of("year")
+        .unwrap_or("0")
+        .trim()
+        .parse()
+        .expect("Day needs to be a number");
     let mut file = args.value_of("file").unwrap_or("").to_string();
     let testing = args.is_present("test");
-
 
     // Log the desire of the user
     println!("Advent of Code 2019 Day {}:", day);
@@ -72,7 +81,6 @@ fn main() {
         file = format!("./{}_data/day{}.txt", dir, day);
     }
 
-    
     println!("Processing: {}", &file);
     run_for_year(year, day, &file);
 }
